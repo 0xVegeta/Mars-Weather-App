a1ce7171b3ad2da2f2759dc6e770784522cdf21f
Anas <61079117+Kadastross@users.noreply.github.com>
2020-10-30 21:36:27 +0530

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+
+
+
+const API_KEY = 'DEMO_KEY'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
+
+
+// Where the cool stuff happens
+
+
+// Change this if you need to
+const previousWeatherToggle = document.querySelector('.show-previous-weather');
+
+const previousWeather = document.querySelector('.previous-weather')
+
+previousWeatherToggle.addEventListener('click', () => {
+    previousWeather.classList.toggle('show-weather')
+} ) 
+
+getWeather()
+
+function getWeather ()
+{ 
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(data => 
+            { 
+                console.log(data);
+            }
+
+}
+
+
+
+
